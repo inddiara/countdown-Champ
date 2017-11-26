ec44fc8191f8411587c25d8ed24c5e8470244a16
inddiara <indiara96d@gmail.com>
2017-11-26 00:26:35 -0200

    Countdown finished

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -20,6 +20,10 @@ componentDidMount() {
   setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
 }
 
+leading0(num) {
+  return num < 10 ? '0' + num : num;
+}
+
 getTimeUntil(deadline) {
   const time = Date.parse(deadline) - Date.parse(new Date());
   const seconds = Math.floor((time/1000)%60);
@@ -35,10 +39,10 @@ getTimeUntil(deadline) {
       return (
     <div>
       <div>
-        <div className="Clock-days">{this.state.days} days</div>
-        <div className="Clock-hours">{this.state.hours} hours</div>
-        <div className="Clock-minutes">{this.state.minutes} minutes</div>
-        <div className="Clock-seconds">{this.state.seconds} seconds</div>
+        <div className="Clock-days">{this.leading0(this.state.days)} days</div>
+        <div className="Clock-hours">{this.leading0(this.state.hours)} hours</div>
+        <div className="Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+        <div className="Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
       </div>
     </div>
   )
